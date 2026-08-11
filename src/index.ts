90363000c252765d97f3af965d5ff32c94ef88a5
BrenoFreitasM <brenofreitasmendes@gmail.com>
2026-08-11 19:40:10 -0300

    feat: add additional CORS origins for local development

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,8 @@ app.use(
       "http://127.0.0.1:3000",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "http://localhost:8080",
+      "http://127.0.0.1:8080",
     ],
     allowHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
     allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
